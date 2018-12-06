b28ac573c840656f826c91acd1ce4a35324bfd5a
lucky1506 <lucky.pataky@gmail.com>
2018-12-05 17:47:44 -0800

    Made a new repository, and a new first testfile to check pyCharm and git setup is complete.

---
+++ create_new_file_LUCKY.py
@@ -0,0 +1,22 @@
+def create_new_file(file_name):
+    """
+    This function takes a name for a file and creates a new student data file
+    with comma separated 4 columns, and with its 1st row as the header for
+    Student Name, ID, Marks and Letter Grade. User may enter student data in
+    this new file before closing or quit to close the new file.
+    """
+
+    new_file = open(file_name, "w")
+    print("\nYou have created a new file named: " + file_name + "\n")
+    # write the 1st row which is the Header in 4 columns
+    new_file.write("Student_Name,ID,Marks,Grade\n")
+    entry = int(input("Do you want to enter data? 1 = Y, 0 = N: \n"))
+
+    while entry != 0:
+        student_name = input("Enter Student Name: \n")
+        id = input("Enter Student ID: \n")
+        marks = input("Enter Student Marks: \n")
+        grade = calculate_letter_grade(marks)
+        new_file.write(student_name + "," + id + "," + marks + "," + grade + "\n")
+        entry = int(input("\n\nDo you want to enter another data? 1 = Y, 0 = N: "))
+    new_file.close()
---
+++ is_all_digit_LUCKY.py
@@ -0,0 +1,12 @@
+
+def is_all_digit(user_input):
+    """
+    This function validates user entry.
+    It checks if entry is only digits
+    from 0 to 9, and no other characters.
+    """
+    numbers = "0123456789"
+    for character in user_input:
+        if character not in numbers:
+            return False
+    return True
