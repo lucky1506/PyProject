ec8a90effb28ef2ae59ce1c6dbd760132ab84f04
Stephanie Boyette <sjboyette@gmail.com>
2018-12-12 18:24:21 -0800

    testing - wip-Item11_student_data_data_manager_app_STEPH.py

--- Item11_student_data_manager_app_STEPH.py
+++ Item11_student_data_manager_app_STEPH.py
@@ -19,41 +19,109 @@ def get_non_empty_input(question):
     return value
 
 
-#  Item 13
+#  Item 13          sjb alsmost fixed
 def write_from_dictionary_to_file(d, filename):
     """ This method writes comma separated values from dictionary to file"""
+    """
+    This check should live in write to dict function
+    Check three things before over-writing to file:
+        if file has header and atleast one student so two lines in file,
+            add those two lines, incliding header in dict 
 
-    # open the file in write mode
-    f = open(filename, "w")
+        if file empty?    (maybe change code in write to dict funciton)
+            grab header line in dict eg. key == string ID, copy first to file
+            then append the rest of the the dict to file. (write append feature function?)
 
-    # if the id (key) exists in the dictionary, write the key and values in the file
+        if file has header line from when it was generated ONLY
+            append the rest of the dict or write header in dict 
+
+
+         NOTE: we need two kinds of write to file modes one for append and one for over-write
+         
+                if option append mode, seek() the end of the file to append   
+
+    """
+
+    print("=============================================")
+    print("dictionary object: ", d)
+    print("file name: ", filename)
+    print("=============================================")
+
+    #open the file in write mode
+    f = open(filename,'a')
+
+    #f.seek(2)
+
+    ####### if key == "id"
+            #  f.    
+
+    #if the id (key) exists in the dictionary, write the key and values in the file
     for key in d:
-        value_list = d[key]
-        f.write(key + "," + value_list[0] + "," + value_list[1] + "," + value_list[2] + "\n")
+        #if f.read(2) == '\n\n':  
+        file_value_part = d[key]            
+        print("++++++++++++++++++++++++++++++++++++++++++++++")
+        
+    
+
+        f.write(key + "," + file_value_part["Student Name"] + "," + str(file_value_part["Final Marks"]) + "," + file_value_part["Final Grade"] + "\n")
+
+        print("++++++++++++++++++++++++++++++++++++++++++++++")
+    
     f.close()
 
 
 
-#  Item 12
+#  Item 12            sjb                   ########################################################################
 def load_csv_file_to_dictionary(fname):
+    """
+    This check should live in write to dict function
+    Check three things before over-writing to file:
+        if file has header and atleast one student so two lines in file,
+            add those two lines, incliding header in dict 
+
+        if file empty?    (maybe change code in write to dict funciton)
+            grab header line in dict eg. key == string ID, copy first to file
+            then append the rest of the the dict to file. (write append feature function?)
+
+        if file has header line from when it was generated ONLY
+            append the rest of the dict or write header in dict 
+
+
+         NOTE: we need two kinds of write to file modes one for append and one for over-write
+         
+                if option append mode, seek() the end of the file to append   
+
+    """
     student_id_data = {}
+
+
+    print(student_id_data)
+
 
     f = open(fname, "r")
     lines = f.readlines()
     f.close()
     data_lines = lines[1:]
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    print("data_lines: ", data_lines)
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 
     for line in data_lines:
         line = line.strip()
         words = line.split(",")
-        student_names = words[0]
-        student_id = words[1]
+        student_names = words[1]           # changed from [0] to [1]
+        student_id = words[0]              # chaned from words[1] to [0]
+   
+        print("student_id: ", student_id)
+        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^")
         final_marks = float(words[2])
         final_grade = words[3]
         student_id_data[student_id] = {}
         student_id_data[student_id]["Student Name"] = student_names
         student_id_data[student_id]["Final Marks"] = final_marks
         student_id_data[student_id]["Final Grade"] = final_grade
+
+    print("Test load_csv_file_ditionary: ",student_id_data)
 
     return student_id_data
 
@@ -97,7 +165,7 @@ def calculate_letter_grade(marks):
         return "A"
 
 
-#  Item 1
+#  Item 1     ######## SJB CALL write to dictionary here after file data has been entered
 def create_new_file(file_name):
     """
     This function takes a name for a file and creates a new student data file
@@ -124,10 +192,16 @@ def create_new_file(file_name):
     new_file.close()
 
 
+    ##### call write() to dic here
+
+
 # Item 2
 def update_file(file_to_update):
 
     dictionary_file = load_csv_file_to_dictionary(file_to_update)
+    print("&&&&&&&&&&&&&&&&&&&&&&&&&&")
+    print("load_csv_file_to_dictionary:",dictionary_file)
+    print("&&&&&&&&&&&&&&&&&&&&&&&&&&")
 
     print("Enter 1 - to add a New Student\n" +
           "Enter 2 - to modify a student Name\n" +
@@ -135,24 +209,46 @@ def update_file(file_to_update):
           "Enter 4 - to remove a Student\n" +
           "Enter 0 - to exit\n")
     choice = int(get_non_empty_input("Enter a selection: \n"))
+    print("{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{")
+    print("user choice: ", choice)
+    print("{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{")
 
     while choice != 0:
         if choice == 1:
             new_student_name = get_non_empty_input("Enter New Student Name: \n")
+            #print("new_student_name: ", new_student_name)  #
             id = get_non_empty_input("Enter Student ID: \n")
+            #print("student ID: ", id)  #
             marks = get_non_empty_input("Enter Student Marks: \n")
+            #print("student marks: ", marks)   #
             grade = calculate_letter_grade(float(marks))
+            print("Student grade: ", grade)   #
 
-            dictionary_file[id] = {}
+            dictionary_file[id] = {} 
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("dictionary_file: ", dictionary_file) #
+
             dictionary_file[id]["Student Name"] = new_student_name
             dictionary_file[id]["Final Marks"] = marks
             dictionary_file[id]["Final Grade"] = grade
+            print("dict object final grade: ", grade)
+
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
             write_from_dictionary_to_file(dictionary_file,file_to_update)
+
+            print("update_file dictionary: ", dictionary_file, "##############") 
 
         elif choice == 2:
             key_id = get_non_empty_input("Enter the Student ID: \n")
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            print("key_id", key_id)
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
             if key_id in dictionary_file:
-                dictionary_file[key_id]["Student Name"] = get_non_empty_input("Enter the new student name: \n")
+                dictionary_file[key_id]["Student Name"] = get_non_empty_input("Enter the new student name: \n")       ###
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                print("dict file object key: ", dictionary_file[key_id]["Student Name"])
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                 write_from_dictionary_to_file(dictionary_file, file_to_update)
 
         elif choice == 3:
@@ -208,7 +304,9 @@ def student_data_manager(user_input):
 
     # check for valid menu selections
     if is_all_digit(user_input) == False:
+        print("$$$$$$$$$$$$$$$$$$$$$$$$")
         print("\nInvalid selection.\n")
+        print("$$$$$$$$$$$$$$$$$$$$$$$$")
         return 1  # invalid selection
 
     # convert the user_input to int since the input function reads input as string
@@ -230,7 +328,10 @@ def student_data_manager(user_input):
         #  Update File
         elif user_input == 2:
             file_to_update = input("Enter the file name you want to update: ")
-            update_file(file_to_update)
+            print("#################################################################################")
+            print("this is the file to update: ", file_to_update)
+            print(update_file(file_to_update), "##################") 
+            print("#################################################################################")                                     ########
 
         #  Remove Student
         elif user_input == 3:
@@ -257,7 +358,7 @@ def student_data_manager(user_input):
 
     else:
         print("Invalid entry.")
-    return 3  # valid selection and entry = 1 to 6
+    return 3  # valid selection and entry = 1 to 6         ###### why are we entering 3 here instead pf calling the function again
 
 
 #  Item 10
